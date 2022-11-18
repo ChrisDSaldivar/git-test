2b031503b7b37bbc4977bfc562efb2aa6f5872f5
Christopher Saldivar <chrisdsaldivar@gmail.com>
2022-11-18 14:08:11 -0600

    Failed to check if the file existed before opening in read mode.

--- script.py
+++ script.py
@@ -1,3 +1,13 @@
-print("Hello World!")
+filename = "data.txt"
 
-sys.exit(2)
+data_file = open(filename, "r")
+line = data_file.readline().strip()
+
+while line != "":
+    val = int(line)
+    if val > 10:
+        print(val)
+        
+    line = data_file.readline().strip()
+
+data_file.close()
