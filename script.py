d849e9bb6a88ee078f7003aff78048ed28c4a813
Christopher Saldivar <chrisdsaldivar@gmail.com>
2022-11-19 13:44:27 -0600

    still broken

--- script.py
+++ script.py
@@ -3,11 +3,13 @@ filename = "data.txt"
 data_file = open(filename, "r")
 line = data_file.readline().strip()
 
+count = 0
 while line != "":
     val = int(line)
     if val > 10:
-        print(val)
-        
+        count += 1
+
     line = data_file.readline().strip()
 
 data_file.close()
+print(f"There are {count} numbers over 10")
